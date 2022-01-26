080173f8acf5377ee18a1283f5b10cf950ff3e85
Vikram Negi <vikramnegi175@gmail.com>
2022-01-26 14:14:48 +0530

    first commit

---
+++ select_html_ele.js
@@ -0,0 +1,13 @@
+// console.log("Script Loaded")
+// console.log(document.getElementById("navbar"));
+
+// console.log(document.getElementsByClassName("boxes"));
+// console.log(document.getElementsByTagName("p"))
+
+// Query Selector
+// Query Selector allow to use css selector to select html element . Its a new way in JavaScript tp select HTML elements
+
+console.log(document.querySelectorAll('.boxes img'))
+console.log(document.querySelector('.boxes img'))
+
+console.log(document.querySelector('#box-container'))
